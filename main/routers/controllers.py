5b5991b567114d48910fa618fdc9e5ca638b76f2
miyahara <50367145+subretu@users.noreply.github.com>
2022-03-12 21:58:26 +0900

    バックエンド側からのデータを表示

--- main/routers/controllers.py
+++ main/routers/controllers.py
@@ -14,3 +14,17 @@ jinja_env = templates.env
 @router.get("/")
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/barchart")
+def barchart(request: Request):
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "values": values,
+            "labels": labels,
+        },
+    )
