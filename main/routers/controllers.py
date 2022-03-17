b38aca2e57337ac3d3232b26a72fe8187afb005f
miyahara <50367145+subretu@users.noreply.github.com>
2022-03-17 22:25:15 +0900

    ルーティングで表示内容を変更

--- main/routers/controllers.py
+++ main/routers/controllers.py
@@ -23,7 +23,7 @@ def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@router.get("/barchart")
+@router.get("/timebarchart")
 def barchart(request: Request):
     conn = get_connection()
     cur = conn.cursor()
@@ -70,6 +70,52 @@ def barchart(request: Request):
         },
     )
 
+@router.get("/barchart")
+def barchart(request: Request):
+    conn = get_connection()
+    cur = conn.cursor()
+    result_data_time = get_time_summary(cur)
+    result_data_day = get_date_summary(cur)
+    result_data_day_stack = get_date_stack_summary(cur)
+
+    labels1, labels2, values1, values2, values3, values4, values5 = (
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+    )
+    for item1, item2 in result_data_time:
+        labels1.append(item1)
+        values1.append(item2)
+
+    for item1, item2 in result_data_day:
+        labels2.append(item1)
+        values2.append(item2)
+
+    for item1, item2, item3, item4 in result_data_day_stack:
+        values3.append(item2)
+        values4.append(item3)
+        values5.append(item4)
+
+    cur.close()
+    conn.close()
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "values1": values1,
+            "labels1": labels1,
+            "values2": values2,
+            "labels2": labels2,
+            "values3": values3,
+            "values4": values4,
+            "values5": values5,
+        },
+    )
 
 @router.get("/day")
 def get_day(request: Request):
